644f419d864aa3d429a36cd3978dbf519e1e1beb
Adam Beyer <abeyerdev@gmail.com>
2017-06-05 20:51:07 -0500

    added state use for search bar

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
-    render() {
-        return <input />;
+    constructor(props) {
+        super(props);
+
+        // this is the only place we explicitly assign state; only class-based components have state
+        this.state = { term: '' }
     }
 
-
+    render() {
+        return (
+            <div>
+                <input
+                    // this turns the input into a controlled input, form element
+                    value = { this.state.term } 
+                    // this.setState causes component to rerender
+                    onChange = { event => this.setState({ term: event.target.value }) } 
+                />
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
